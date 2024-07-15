270cd9c9e108bcf3154f41c19b22186a20770020
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-07-15 22:25:40 +0530

    Added Menu

--- src/Components/MainContainer/mainContainer.js
+++ src/Components/MainContainer/mainContainer.js
@@ -2,15 +2,33 @@ import React, {useContext, useEffect} from "react";
 import "./mainContainer.css";
 import { DataContext } from "../../ContextAPI/dataContext";
 import ChatField from "../ChatField/chatField";
+import Menu from "../Menu/menu";
+
 import { useState } from "react";
 const MainContainer = () => {
-   const {activeConversation, setConversations} = useContext(DataContext); 
+   const {activeConversation,setActiveConversation, setConversations} = useContext(DataContext); 
    const [chatIndex, setChatIndex] = useState(-1);
 
    useEffect (() => {
     const chatHistory = JSON.parse(localStorage.getItem("chatHistory")) || [];
     setConversations(chatHistory);
    },[setConversations])
+
+   const handleNewChat = () => {
+    if (chatIndex !== -1){
+        setActiveConversation([]);
+        localStorage.setItem("activeConversation",JSON.stringify([]));
+        setChatIndex(-1)
+    }
+    else if(activeConversation.length > 0 ){
+        const chatHistory = JSON.parse(localStorage.getItem("chatHistory")) || [];
+        chatHistory.push(activeConversation);
+        localStorage.setItem("chatHistory",JSON.stringify(chatHistory));
+        setConversations(chatHistory);
+        setActiveConversation([]);
+        localStorage.setItem("activeConversation",JSON.stringify([]));
+    }
+   }
 
    const handleChatSave = () => {
     if(chatIndex !== -1){
@@ -23,6 +41,7 @@ const MainContainer = () => {
    }
     return(
         <div className="chatbot-page">
+            <Menu handleNewChat={handleNewChat} setChatIndex={setChatIndex}/>
             <ChatField handleChatSave={handleChatSave}/>
         </div>
     )
---
+++ src/Components/Menu/menu.js
@@ -0,0 +1,96 @@
+import React, { useState, useContext } from "react";
+import "./menu.css";
+import AiIcon from "../../Assets/ai-icon.png";
+import EditIcon from "../../Assets/edit.png";
+import { DataContext } from "../../ContextAPI/dataContext";
+import { RiDeleteBinFill } from "react-icons/ri";
+const Menu = ({ handleNewChat, setChatIndex }) => {
+    const {activeConversation, setActiveConversation, conversations, setConversations } = useContext(DataContext);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const handleChat = (index) => {
+        return () => {
+            // console.log("conversationlist", conversations[index]);
+            setActiveConversation(conversations[index]);
+            localStorage.setItem("activeConversation",JSON.stringify(activeConversation));
+            setChatIndex(index);
+            setMenuOpen(false);
+        };
+    };
+    
+    const handleDelete = (index) => {
+        const chatHistory = JSON.parse(localStorage.getItem("chatHistory"));
+        chatHistory.splice(index, 1);
+        console.log(chatHistory);
+        localStorage.setItem("chatHistory", JSON.stringify(chatHistory));
+        setConversations(chatHistory);
+        setActiveConversation([]);
+        localStorage.setItem("activeConversation",JSON.stringify([]));
+    }
+
+    const handleNewChatAction = () => {
+        handleNewChat()
+        setMenuOpen(false);
+    }
+
+    return (
+        <div>
+            <div className="menu">
+                <div className="new-chat-btn" onClick={handleNewChatAction}>
+                    <img style={{ width: "32px", height: "32px" }} src={AiIcon} alt="img" />
+                    <div className="new-chat-text">New Chat</div>
+                    <img className="edit-icon" src={EditIcon} alt="" />
+                </div>
+                <div className="conversation-list">
+                    {conversations.map((conversation, index) => (
+                        <div key={index} className="conversation-item">
+                            <div 
+                                style={{cursor:"pointer"}} 
+                                onClick={handleChat(index)}>
+                                Conversation {index + 1}
+                            </div>
+                            <RiDeleteBinFill 
+                                style={{cursor:"pointer"}} 
+                                onClick={() => handleDelete(index)}/>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="menu-mobile-screen">
+                <div className="menu-navbar">
+                    <div className="menu-icon" onClick={toggleMenu}>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                    </div>
+                    <div className="app-title">Bot AI</div>
+                </div>
+                {menuOpen && (
+                    <div className="menu-mobile-view">
+                        <div className="new-chat-btn" onClick={handleNewChatAction}>
+                            <img style={{ width: "32px", height: "32px" }} src={AiIcon} alt="img" />
+                            <div className="new-chat-text">New Chat</div>
+                            <img className="edit-icon" src={EditIcon} alt="" />
+                        </div>
+                        <div className="conversation-list">
+                            {conversations.map((conversation, index) => (
+                                <div key={index} className="conversation-item">
+                                    <div className="" style={{cursor:"pointer"}} onClick={handleChat(index)}>
+                                        Conversation {index + 1}
+                                    </div>
+                                    <RiDeleteBinFill style={{cursor:"pointer"}} onClick={() => handleDelete(index)}/>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Menu;

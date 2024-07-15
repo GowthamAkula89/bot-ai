dd95a78ae841080c50264d3facaa20fa7d598052
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-07-15 21:57:58 +0530

    Added ChatField

---
+++ src/Components/ChatField/chatField.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import "./chatField.css";
+import { DataContext } from "../../ContextAPI/dataContext";
+import AiIcon from "../../Assets/ai-icon.png";
+
+const ChatField = ({ handleChatSave }) => {
+    const { activeConversation, setActiveConversation } = useContext(DataContext);
+    const [question, setQuestion] = useState("");
+    const conversationData = require("../../Data/chatConversations.json");
+    
+    const chatFieldRef = useRef(null);
+
+    useEffect(() => {
+        const savedActiveConversation = JSON.parse(localStorage.getItem("activeConversation")) || [];
+        setActiveConversation(savedActiveConversation);
+        setTimeout(scrollToBottom, 0);
+    }, [setActiveConversation]);
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [activeConversation]);
+
+    const scrollToBottom = () => {
+        if (chatFieldRef.current) {
+            chatFieldRef.current.scrollTop = chatFieldRef.current.scrollHeight;
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (question.trim() !== "") {
+            let answer = conversationData.find((que) => que.question.toLowerCase() === question.toLowerCase()) || null;
+            if (answer === null) {
+                answer = conversationData.find((que) => que.question.toLowerCase().includes(question.toLowerCase())) || { response: "Add some more details to understand" };
+            }
+            const newConversation = { question: question, answer: answer.response, rating : 0, feedback : ""};
+            setActiveConversation([...activeConversation, newConversation]);
+            setQuestion("");
+            localStorage.setItem("activeConversation", JSON.stringify([...activeConversation, newConversation]));
+            setTimeout(scrollToBottom, 0);
+        }
+    };
+
+    const handleQuestionChange = (e) => {
+        setQuestion(e.target.value);
+    };
+    
+    
+    return (
+        <div className="chat-qna-response">
+            <div className="chat-field" ref={chatFieldRef}>
+                <div className="app-title">Bot AI</div>
+                <div className="app-hero-section">
+                    <div className="hero-section-text">How Can I Help You Today?</div>
+                    <img src={AiIcon} alt="logo-ai" />
+                </div>
+
+                {activeConversation.map((conversation, index) => (
+                    <div key={index}>
+                    </div>
+                ))}
+            </div>
+            <form className="input-field" onSubmit={handleSubmit}>
+                <input
+                    className="input-value"
+                    type="text"
+                    placeholder="Message BOT AI"
+                    value={question}
+                    onChange={handleQuestionChange}
+                />
+                <button className="action-button" type="submit">Ask</button>
+                <button className="action-button" type="button" onClick={handleChatSave}>Save</button>
+            </form>
+        </div>
+    );
+};
+
+export default ChatField;
--- src/Components/MainContainer/mainContainer.js
+++ src/Components/MainContainer/mainContainer.js
@@ -1,11 +1,29 @@
 import React, {useContext, useEffect} from "react";
 import "./mainContainer.css";
 import { DataContext } from "../../ContextAPI/dataContext";
+import ChatField from "../ChatField/chatField";
+import { useState } from "react";
 const MainContainer = () => {
-   
+   const {activeConversation, setConversations} = useContext(DataContext); 
+   const [chatIndex, setChatIndex] = useState(-1);
+
+   useEffect (() => {
+    const chatHistory = JSON.parse(localStorage.getItem("chatHistory")) || [];
+    setConversations(chatHistory);
+   },[setConversations])
+
+   const handleChatSave = () => {
+    if(chatIndex !== -1){
+        const chatHistory = JSON.parse(localStorage.getItem("chatHistory"))
+        chatHistory[chatIndex] = activeConversation;
+        console.log("chatHistory",chatHistory)
+        localStorage.setItem("chatHistory",JSON.stringify(chatHistory));
+        setConversations(chatHistory);
+    }  
+   }
     return(
         <div className="chatbot-page">
-            
+            <ChatField handleChatSave={handleChatSave}/>
         </div>
     )
 }

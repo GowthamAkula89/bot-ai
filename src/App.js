68dd5eeede6645b3101fe216084bd1a6ced8401c
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-07-24 23:42:07 +0530

    Implemented Store Provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import MainContainer from './Components/MainContainer/mainContainer';
-import DataProvider from './ContextAPI/dataContext';
-
+//import DataProvider from './ContextAPI/dataContext';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 function App() {
   return (
     <div className="App">
-      <DataProvider>
+      <Provider store={store}>
         <MainContainer/>
-      </DataProvider>
+      </Provider>
     </div>
   );
 }
